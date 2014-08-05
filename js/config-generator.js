c93aa419c8832ed1be123f9b4a50f3c3ad52776b
Doug <>
2014-08-05 16:45:04 -0600

    close to generating tree

--- js/config-generator.js
+++ js/config-generator.js
@@ -2,24 +2,38 @@ var ConfigGenerator = (function(){
   function generateConfigFromEntry(entry){
     return new Promise(function(resolve, reject){
       FileSystem.getFilesRecursive(entry).then(function(files){
-        var configEntries = [];
-        for(var i = 0; i < files.length; i++){
-          var configEntry = {};
-          configEntry.path = files[i].fullPath;
-          configEntries.push(configEntry);
-        }
-        resolve(JSON.stringify(configEntries));
+        var pathTree = createPathTree(files);
+        config = createConfigFromPathTree("", pathTree);
+        resolve(JSON.stringify(config));
       });
     });
   }
   function createPathTree(files){
-    var tree = {};
+    var root = TreeNode.create("");
     for(var i = 0; i < files.length; i++){
-      var pathComponents = files[i].split("/");
+      var pathComponents = files[i].fullPath.split("/");
+      var currentNode = root;
       for(var j = 0; j < pathComponents.length; j++){
-        tree[pathComponents[i]]
+        if(!currentNode.hasChild(pathComponents[j])){
+          currentNode.appendChild(TreeNode.create(pathComponents[j]));
+        }
+        currentNode = currentNode.getChild(pathComponents[j]);
       }
     }
+    return root;
+  }
+  function createConfigFromPathTree(pathName, pathTree){
+    var path = pathName + "/" + pathTree.name;
+    var item = {};
+    item.path = path;
+    if(pathTree.children.length > 0){
+      item.subtests = [];
+      for(var i = 0; i < pathTree.children.length; i++){
+        var subtest = createConfigFromPathTree(path, pathTree.children[i]);
+        item.subtests.push(subtest);
+      }
+    }
+    return item;
   }
   return {
     generateConfigFromEntry : generateConfigFromEntry
